7dbd0211c60b81d2c527c36317028aaf2500b3ee
Jstrode39 <172228083+Jstrode39@users.noreply.github.com>
2025-04-16 14:22:31 -0400

    Track dual-token rewards in staking: SOL + IPLR

--- projects/IPLR/index.js
+++ projects/IPLR/index.js
@@ -1,30 +1,17 @@
 const { getTokenBalance } = require('../helper/solana')
 
 const SOL_MINT = 'So11111111111111111111111111111111111111112'
-const LP_ACCOUNT = 'GpMZbSM2GgvTKHJirzeGfMFoaZ8UR2X7F4v8vHTvxFbL'
+const IPLR_MINT = 'EyzgnBfHGe9hh169B8993muBVcoeURCnSgPbddBeSybo'
 const REWARDS_WALLET = 'EftM2RPnZqLMc3UpE7Xjz41DdtAopYziCmB4r1wVAJ9C'
 
-async function pool2() {
-  const solBalance = await getTokenBalance(SOL_MINT, LP_ACCOUNT)
-  return {
-    'solana:So11111111111111111111111111111111111111112': solBalance
-  }
-}
-
 async function staking() {
-  const solBalance = await getTokenBalance(SOL_MINT, REWARDS_WALLET)
-  return {
-    'solana:So11111111111111111111111111111111111111112': solBalance
-  }
-}
+  const [solBalance, iplrBalance] = await Promise.all([
+    getTokenBalance(SOL_MINT, REWARDS_WALLET),
+    getTokenBalance(IPLR_MINT, REWARDS_WALLET),
+  ])
 
-module.exports = {
-  timetravel: false,
-  misrepresentedTokens: true,
-  methodology: 'Tracks SOL in the IPLR/SOL LP vault (pool2) and in the rewards wallet (staking).',
-  solana: {
-    tvl: () => ({}),
-    pool2,
-    staking
+  return {
+    'solana:So11111111111111111111111111111111111111112': solBalance,
+    'solana:EyzgnBfHGe9hh169B8993muBVcoeURCnSgPbddBeSybo': iplrBalance
   }
 }
